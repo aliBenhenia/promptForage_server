15a9b34ce4aa6e8c1656262fb9e8fd3f46a04d73
aliBenhenia <alibenhenia1@gmail.com>
2025-05-13 19:28:33 +0200

    chore: update environment configuration and remove example files

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/promptforge', {
+mongoose.connect("mongodb+srv://alibenhenia1:3TkEK63GFAfL8ZZf@cluster0.l3xb1ca.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
+const SecretKey = process.env.JWT_SECRET || 'your_default_secret_key';
 const auth = async (req, res, next) => {
   try {
     // Get token from header
@@ -14,7 +15,7 @@ const auth = async (req, res, next) => {
     // geneaarte secret key random now
     // const secretKey = crypto.randomBytes(32).toString('hex');
     console.log('Secret Key:', process.env.JWT_SECRET);
-    const decoded = jwt.verify(token, process.env.JWT_SECRET || 'your_jwt_secret');
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     
     // Find user
     const user = await User.findById(decoded.id);
