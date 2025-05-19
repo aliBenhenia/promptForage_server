b562ce3c259dc841d3effea2aac2a17c07aba086
aliBenhenia <alibenhenia1@gmail.com>
2025-05-19 17:58:35 +0200

    update

--- client/lib/api.ts
+++ client/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Default to mock server in development 
-const API_URL =   'http://localhost:5000/';
+const API_URL =  process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api';
  
 const api = axios.create({
   baseURL: API_URL,
